47d5b73dd372dc01424273f9da3042e96b81593e
lochlynlaskowski <lochlyn.laskowski@gmail.com>
2022-08-28 12:22:10 -0500

    day three SQL

---
+++ SQL_practice_day_three.sql
@@ -0,0 +1,68 @@
+1.
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+SELECT user_id, CONCAT(upper(left(name,1)), lower(right(name,length(name)-1))) as name
+FROM Users
+ORDER BY user_id;
+
+2.
+Table Activities:
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+ 
+
+Write an SQL query to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+
+SELECT sell_date, COUNT(DISTINCT product) AS num_sold,  
+GROUP_CONCAT(DISTINCT product) AS products
+FROM Activities 
+GROUP BY sell_date
+ORDER BY sell_date;
+
+3.
+Table: Patients
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+ 
+
+Write an SQL query to report the patient_id, patient_name all conditions of patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix
+
+Return the result table in any order.
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE '%DIAB1%'
